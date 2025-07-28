6629f85d40b9d3b613d54f5f661b30db035ff693
Victor-Hck <victor.profissional3592@gmail.com>
2025-07-28 16:06:04 -0300

    first commit

---
+++ ex003/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $nome = "victor";
+        $_sobrenome = "Antônio";
+        $idade = 23;
+        $peso = 118.5;
+        $_casado = false;
+        const PAIS = "Brasil";
+        
+        $nomeCompletoCliente = "Camel Case";
+        $telefone_contato_fornecedor = "Snake Case";
+        $nomecursosuperior = "";
+        echo "Muito prazer, $nome $_sobrenome! Você mora no " . PAIS;
+    ?>
+</body>
+</html>
