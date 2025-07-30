cb5d3748d98b3c2784815aa5c3ab0bf07489ce0a
Victor-Hck <victor.profissional3592@gmail.com>
2025-07-30 14:58:13 -0300

    add desafio1

---
+++ desafio1/cad.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <head>
+        
+    </head>
+    <main>
+        <h1>Resultado Final</h1>
+        <?php 
+            $numberValue = $_GET["number"];
+            $valorInteiro = intval($numberValue);
+            $antecessor = $valorInteiro - 1;
+            $sucessor = $valorInteiro + 1;
+            echo "O número escolhido foi <strong>$valorInteiro</strong><br>O seu antecessor é $antecessor<br>O seu sucessor é $sucessor<br>";
+        ?>
+        <butoon><a href="javascript:history.go(-1)">Voltar</a></butoon>
+    </main>
+</body>
+</html>
