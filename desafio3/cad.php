be6c42ad311c58c7b1cdb66d6baadeac4dc62d1f
Victor-Hck <victor.profissional3592@gmail.com>
2025-08-26 15:16:32 -0300

    update

--- desafio3/cad.php
+++ desafio3/cad.php
@@ -13,9 +13,9 @@
             const DOLAR = 5.22;
             $valorpegar = sprintf("%.2f", $_GET["number"]);
             $valorconverter = floatval($valorpegar);
-            $valorconvertido = sprintf("%.2f", $valorconverter / dolar);
+            $valorconvertido = sprintf("%.2f", $valorconverter / DOLAR);
             echo "Seus R$ $valorconverter equivalem a <strong>US$ $valorconvertido</strong><br>";
-            echo "<br><strong>*cotação fixa de " . dolar . "</strong> informada diretamente no código.";
+            echo "<br><strong>*cotação fixa de " . DOLAR . "</strong> informada diretamente no código.";
         ?>
         <br><br>
         <a href="javascript:history.go(-1)"><button>Voltar</button></a>
---
+++ desafio6/cad.php
@@ -0,0 +1,16 @@
+<?php 
+// Desafio 6 – Anatomia de uma Divisão: o aluno deve simular uma divisão com quociente e resto,
+// organizando visualmente os dados do cálculo, incluindo dividendo, divisor, resultado e sobra;
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../desafio6/style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
